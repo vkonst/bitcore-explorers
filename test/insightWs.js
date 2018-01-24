6f2a27f163df5f1a04d4f4be86c61d104c8c1571
vkonst <vadim.konstantinov@gmail.com>
2018-01-24 18:22:20 +0300

    InsightWs "constructor" (and tests) fixed

--- test/insightWs.js
+++ test/insightWs.js
@@ -20,16 +20,16 @@ describe('InsightWs', function () {
             should.exist(insightWs.url);
             should.exist(insightWs.network);
             if (insightWs.network === Networks.livenet) {
-                insightWs.url.should.equal('https://insight.bitpay.com/insight-api/');
-                insightWs.wsUrl.should.equal('https://insight.bitpay.com');
+                insightWs.url.should.equal('https://insight.bitpay.com/api/');
+                insightWs.wsUrl.should.equal('https://insight.bitpay.com/');
             } else if (insightWs.network === Networks.testnet) {
-                insightWs.url.should.equal('https://test-insight.bitpay.com/insight-api/');
-                insightWs.wsUrl.should.equal('https://test-insight.bitpay.com');
+                insightWs.url.should.equal('https://test-insight.bitpay.com/api/');
+                insightWs.wsUrl.should.equal('https://test-insight.bitpay.com/');
             }
         });
         it('can be created providing just a network', function() {
             var insightWs = new InsightWs(Networks.testnet);
-            insightWs.url.should.equal('https://test-insight.bitpay.com/insight-api/');
+            insightWs.url.should.equal('https://test-insight.bitpay.com/api/');
             insightWs.wsUrl.should.equal('https://test-insight.bitpay.com/');
             insightWs.network.should.equal(Networks.testnet);
         });
@@ -53,16 +53,18 @@ describe('InsightWs', function () {
         it('can be created with a custom serverUrl that misses ending slash', function() {
             var serverUrl = 'https://localhost:1234';
             var updatedUrl = serverUrl + '/';
+            var urlWithPrefix = serverUrl + '/insight-api/';
             var insightWs = new InsightWs(serverUrl, Networks.testnet);
+            insightWs.url.should.equal(urlWithPrefix);
             insightWs.wsUrl.should.equal(updatedUrl);
             insightWs.network.should.equal(Networks.testnet);
         });
         it('can be created with a custom serverUrl and network', function() {
-            var serverUrl = 'https://localhost:1234';
-            var urlWithPrefix = serverUrl + '/insight-api/';
+            var serverUrl = 'https://localhost:1234/';
+            var urlWithPrefix = serverUrl + 'insight-api/';
             var insightWs = new InsightWs(serverUrl, Networks.testnet);
             insightWs.url.should.equal(urlWithPrefix);
-            insightWs.wsUrl.should.equal(serverUrl + '/');
+            insightWs.wsUrl.should.equal(serverUrl);
             insightWs.network.should.equal(Networks.testnet);
         });
         it('defaults to defaultNetwork on a custom serverUrl', function() {
