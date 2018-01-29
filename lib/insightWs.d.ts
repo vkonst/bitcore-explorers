a4f375502d9423086f5af9a2221b4d0ad70861df
Dmitry Tverdov <abvkat91@gmail.com>
2018-01-29 18:49:32 +0300

    Make some bootstrapped typescript type declaration file for insightWs class.

---
+++ lib/insightWs.d.ts
@@ -0,0 +1,26 @@
+declare module "inisghtWs" {
+    const insightWs: InsightWs;
+
+    type subscribeOpts = {block: string|boolean, tx: string|boolean};
+
+    class InsightWs {
+        constructor (
+            serverOrNetwork: string | object, // string or Bitcore-lib Network
+            network: object, // Bitcore-lib Network
+            routePrefix: string
+        );
+
+        public url: string;
+        public wsUrl: string;
+        public network: object;
+        public events: object; // EventEmitter
+        public subsctiptions: subscribeOpts;
+        public subscribe(subscribeOptions: subscribeOpts): void;
+
+        private _onNewBlock(msg: string): void; // msg is block hash string
+        private _onNewTx(msg: object): void; //msg is transaction returned from insight web socket server
+        private getBlock(blockHash: string, callback: Function): Function;
+    }
+
+    export = insightWs;
+}
