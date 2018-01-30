8e5d71a6f00fb93ae24ad060609b8c790c778f8d
Dmitry Tverdov <abvkat91@gmail.com>
2018-01-30 16:40:41 +0300

    Add Insight class declaration and add Insight class to extends of InsightWs class.

--- lib/insightWs.d.ts
+++ lib/insightWs.d.ts
@@ -1,23 +1,46 @@
+declare class Insight {
+    constructor(
+        url?: string | object, // string URL or Bitcore-lib Network
+        network?: object // FIXME try to declare networks from bitcore-lib
+    );
 
-type subscribeOpts = {block: string|boolean, tx: string|boolean};
+    public url: string;
+    public getTransaction(txid: string, callback: insightCallback): insightCallback;
+    
+    public getUtxos(addresses: Array,  // FIXME try to declare addresses from bitcore-lib
+                    callback: insightCallback): insightCallback;
+    
+    public broadcast(transaction: string | object, // FIXME try to declare transactions from bitcore-lib
+                     callback: insightCallback): insightCallback;
+    
+    public address(address: string | object, // FIXME try to declare addresses from bitcore-lib
+                   callback: insightCallback): insightCallback;
 
-declare class InsightWs {
+    protected network: object; // FIXME try to declare networks from bitcore-lib
+    protected requestPost(path: string, data: JSON, callback: Function): void;
+    protected requestGet(path: string, callback: Function): void;
+}
+
+declare class InsightWs extends Insight {
     constructor (
-        serverOrNetwork?: string | object, // string or Bitcore-lib Network
-        network?: object, // FIXME try declare networks from bitcore-lib
+        serverOrNetwork?: string | object, // string URL or Bitcore-lib Network
+        network?: object, // FIXME try to declare networks from bitcore-lib
         routePrefix?: string
     );
 
     public url: string;
     public wsUrl: string;
-    public network: object; // FIXME try declare networks from bitcore-lib
     public events: {on: Function};
-    public subsctiptions: subscribeOpts;
     public subscribe(subscribeOptions?: subscribeOpts): void;
+    public getBlock(blockHash: string, callback: insightCallback): insightCallback;
 
-    private _onNewBlock(msg: string): void; // msg is block hash string
-    private _onNewTx(msg: object): void; //msg is transaction returned from insight web socket server
-    private getBlock(blockHash: string, callback: Function): Function;
+    protected subsctiptions: subscribeOpts;
+    protected network: object; // FIXME try to declare networks from bitcore-lib
+    protected _onNewBlock(msg: string): void; // msg is block hash string
+    protected _onNewTx(msg: object): void; //msg is transaction returned from insight web socket server
 }
+
+type subscribeOpts = {block: string|boolean, tx: string|boolean};
+type insightCallback = (error: any, data: any) => void;
 
 export = InsightWs;
