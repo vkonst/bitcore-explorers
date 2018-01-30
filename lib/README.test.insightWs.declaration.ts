908c8d9ecdc2acb19155b07fcf226eef98baaeee
Dmitry Tverdov <abvkat91@gmail.com>
2018-01-30 14:56:35 +0300

    Add test file for insightWs declaration.

---
+++ lib/README.test.insightWs.declaration.ts
@@ -0,0 +1,23 @@
+import * as InsightWs from './insightWs';
+
+const serverURL = 'http://127.0.0.1:8080'; // websocket server url
+const subscriptions = {
+    block: true,
+    tx: true
+};
+
+let insightWs = new InsightWs(serverURL);
+
+insightWs.subscribe(subscriptions);
+
+insightWs.events.on('connected', () => {
+    console.log("connected");
+});
+
+insightWs.events.on('block', block => {
+    console.log("New block: ", block)
+});
+
+insightWs.events.on('tx', tx => {
+    console.log("New tx: ", tx)
+});
--- lib/insightWs.d.ts
+++ lib/insightWs.d.ts
@@ -1,26 +1,23 @@
-declare module "insightWs" {
-    const insightWs: InsightWs;
 
-    type subscribeOpts = {block: string|boolean, tx: string|boolean};
+type subscribeOpts = {block: string|boolean, tx: string|boolean};
 
-    class InsightWs {
-        constructor (
-            serverOrNetwork: string | object, // string or Bitcore-lib Network
-            network: object, // Bitcore-lib Network
-            routePrefix: string
-        );
+declare class InsightWs {
+    constructor (
+        serverOrNetwork?: string | object, // string or Bitcore-lib Network
+        network?: object, // FIXME try declare networks from bitcore-lib
+        routePrefix?: string
+    );
 
-        public url: string;
-        public wsUrl: string;
-        public network: object;
-        public events: object; // EventEmitter
-        public subsctiptions: subscribeOpts;
-        public subscribe(subscribeOptions: subscribeOpts): void;
+    public url: string;
+    public wsUrl: string;
+    public network: object; // FIXME try declare networks from bitcore-lib
+    public events: {on: Function};
+    public subsctiptions: subscribeOpts;
+    public subscribe(subscribeOptions?: subscribeOpts): void;
 
-        private _onNewBlock(msg: string): void; // msg is block hash string
-        private _onNewTx(msg: object): void; //msg is transaction returned from insight web socket server
-        private getBlock(blockHash: string, callback: Function): Function;
-    }
-
-    export = insightWs;
+    private _onNewBlock(msg: string): void; // msg is block hash string
+    private _onNewTx(msg: object): void; //msg is transaction returned from insight web socket server
+    private getBlock(blockHash: string, callback: Function): Function;
 }
+
+export = InsightWs;
